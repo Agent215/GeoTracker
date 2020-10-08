974392fed89fa7ee6fda80dd2d207129471215d9
Abraham Schultz <Abraham.Schultz@gmail.com>
2020-10-07 23:54:33 -0400

    animate to user on mapOpen

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,15 +1,67 @@
-import * as React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { StyleSheet, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView from "react-native-map-clustering";
 
-export default function MapScreen() {
+import { View } from '../components/Themed';
+
+
+const LATITUDE_DELTA = 0.0922;
+const { width, height } = Dimensions.get('window');
+const SCREEN_HEIGHT = height;
+const SCREEN_WIDTH = width;
+const ASPECT_RATIO = width / height;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const INITIALREGION = {
+  latitude: 39.9526,
+  longitude: -75.16522,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+}
+
+
+const MapScreen = props => {
+  let mapRef = useRef(null);
+
+  /*
+  animateToUser 
+  */
+  const animateToUser = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        let coords = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA
+        };
+        console.log("MapScreen.js/animateToUser - User Coords : " + coords);
+        mapRef.current.animateToRegion(coords, 0);  // why does my linter give me red squiglly lines, yet this runs...
+
+      }, (error) => console.log("MapScreen.tsx/animateToUser() - Got error from navigator.geolocation.getCurrentPosition: " + error));
+  }
+
+  /* run once on component mount */
+  useEffect(() => {
+    animateToUser();
+  }, []);
+
+
   return (
     <View style={styles.container}>
+
       <MapView
+        ref={mapRef}
+        onMapReady={animateToUser}
+        initialRegion={INITIALREGION}
         style={styles.mapStyle}
-        mapType="satellite"  // we can have this toggle to standard view if we like
+        mapType="hybrid"
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        showsMyLocationButton
+        rotateEnabled={false}
+        showsTraffic={false}
+        toolbarEnabled={true}
       />
     </View>
   );
@@ -31,7 +83,9 @@ const styles = StyleSheet.create({
     width: '80%',
   },
   mapStyle: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT,
   }
 });
+
+export default MapScreen;
