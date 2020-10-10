99cf55d2798e3b4a824f8c772fe76dde31aafb8f
Abraham Schultz <Abraham.Schultz@gmail.com>
2020-10-09 21:22:53 -0400

    removed unused import

--- constants/APIkeys.ts
+++ constants/APIkeys.ts
@@ -1,5 +1,3 @@
-import { API } from "aws-amplify";
-
 export const googlePlacesKey = "AIzaSyB_fuGPEjP2hp9-GNXOt-ElFWceKQFFgz4";
 export const geocoderKey = "AIzaSyB_fuGPEjP2hp9-GNXOt-ElFWceKQFFgz4";
 
