91b34a2841426ed1134384e56c526bc108a713fd
aphotis22 <alecphotis@gmail.com>
2020-10-18 12:36:01 -0400

    Icon assets folder

--- assets/Mocked_Data.tsx
+++ assets/Mocked_Data.tsx
@@ -1,11 +1,12 @@
-
+// Current hard coded data that will later be pulled data from EONET API.
+// Taking in Title, ID, Description, LatL, LongL, and an event icon.
 export const events = [
 
-    { title: 'Windy Wally', id: 1, description: 'Tornado', LatL: 11, LongL: 12, icon: 'hello' },
-    { title: 'Smokey', id: 1, description: 'Wild Fire', LatL: 11, LongL: 12, icon: 'hello' },
-    { title: 'Big Wave', id: 1, description: 'Tsunami', LatL: 11, LongL: 12, icon: 'hello' },
-    { title: 'Ice', id: 1, description: 'Ice Berg', LatL: 11, LongL: 12, icon: 'hello' },
-    { title: 'Lava', id: 1, description: 'Volcano', LatL: 11, LongL: 12, icon: 'hello' }
+    { title: 'Thirsty Thursday', id: 1, description: 'Draught', LatL: 11, LongL: 12, icon: '../assets/Icons/Draught.png'},
+    { title: 'Broken Ground', id: 1, description: 'Earthquake', LatL: 11, LongL: 12, icon: '../assets/Icons/Earthquake.png' },
+    { title: 'Big Wave', id: 1, description: 'Tsunami', LatL: 11, LongL: 12, icon: '../assets/Icons/Tsunami2.png' },
+    { title: 'Ice', id: 1, description: 'Ice Berg', LatL: 11, LongL: 12, icon: '../assets/Icons/Iceberg' },
+    { title: 'Lava', id: 1, description: 'Volcano', LatL: 11, LongL: 12, icon: '../assets/Icons/Volcano.png' }
 ]
 
 
