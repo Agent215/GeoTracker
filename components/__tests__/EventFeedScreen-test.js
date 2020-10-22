e9627c8353f201b3e506fb1269380a8955178eb3
Abraham Schultz <Abraham.Schultz@gmail.com>
2020-10-22 19:54:39 -0400

    mocked redux store and navigator for jest tests

--- components/__tests__/EventFeedScreen-test.js
+++ components/__tests__/EventFeedScreen-test.js
@@ -1,10 +1,23 @@
 import * as React from 'react';
 import renderer from 'react-test-renderer';
-
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+import MockedNavigator from '../MockedNavigator'
 import EventFeedScreen from '../../screens/EventFeedScreen';
 
-it(`EventFeedScreen renders correctly`, () => {
-  const tree = renderer.create(<EventFeedScreen/>).toJSON();
 
-  expect(tree).toMatchSnapshot();
-});
+
+describe('Testing eventfeedscreen Component', () => {
+  const initialState = { output: "null" }
+  const mockStore = configureStore()
+  let store, wrapper
+
+  it(`EventFeedScreen renders correctly`, () => {
+    store = mockStore(initialState)
+
+    const comp = <Provider store={store}><MockedNavigator component={EventFeedScreen} /></Provider>
+    const tree = renderer.create().toJSON(comp);
+
+    expect(tree).toMatchSnapshot();
+  });
+})
