59e9756b4a34451ef5c7f58db773448f055a53ac
Abraham Schultz <Abraham.Schultz@gmail.com>
2020-10-27 00:37:58 -0400

    removed useless test

--- components/__tests__/BottomTabNavigator-test.js
+++ components/__tests__/BottomTabNavigator-test.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import renderer from 'react-test-renderer';
 
-import BottomTabNavigator from '../../navigation/BottomTabNavigator';
 
 /**
  * Check if tab navigator is null
