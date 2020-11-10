64791b170fde87465eb25d64f162fe78ec2d2284
roburdash <roburdash@gmail.com>
2020-11-10 11:36:08 -0500

    Updated GIBS urls for historic weather tiles from best available data being most recent and most coverage

--- constants/WeatherUrls.tsx
+++ constants/WeatherUrls.tsx
@@ -11,10 +11,16 @@ export const weatherUrls = {
 //key and value pairs for use with urlTile for changing weather tile overlay
 export const GibsUrls = {
 
-    ["clouds"]: "https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/IMERG_Precipitation_Rate/default/2014-04-09/GoogleMapsCompatible_Level6/{z}/{y}/{x}.png",
-    ["precipitation"]: "https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/MERRA2_ISCCP_Cloud_Albedo_Monthly/default/2014-04-09/GoogleMapsCompatible_Level6/{z}/{y}/{x}.png",
-    ["pressure"]: "https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/MODIS_Terra_Cloud_Top_Pressure_Day/default/2014-04-09/GoogleMapsCompatible_Level6/{z}/{y}/{x}.png",
-    ["wind"]: "https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/Aquarius_Wind_Speed_L3_7Day_Snapshot/default/2014-04-09/GoogleMapsCompatible_Level6/{z}/{y}/{x}.png",
-    ["temp"]: "https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/AIRS_L3_Surface_Air_Temperature_Monthly_Night/default/2014-04-09/GoogleMapsCompatible_Level6/{z}/{y}/{x}.png",
+    //["clouds"]: "https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/IMERG_Precipitation_Rate/default/2014-04-09/GoogleMapsCompatible_Level6/{z}/{y}/{x}.png",
+    ["clouds"]: "https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/VIIRS_SNPP_Cloud_Effective_Radius/default/default/GoogleMapsCompatible_Level7/{z}/{y}/{x}.png",
+    //["precipitation"]: "https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/MERRA2_ISCCP_Cloud_Albedo_Monthly/default/2014-04-09/GoogleMapsCompatible_Level6/{z}/{y}/{x}.png",
+    ["precipitation"]: "https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/IMERG_Precipitation_Rate/default/default/GoogleMapsCompatible_Level6/{z}/{y}/{x}.png",
+    //["pressure"]: "https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/MODIS_Terra_Cloud_Top_Pressure_Day/default/2014-04-09/GoogleMapsCompatible_Level6/{z}/{y}/{x}.png",
+    ["pressure"]: "https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/AIRS_L2_RelativeHumidity_500hPa_Day/default/default/GoogleMapsCompatible_Level6/{z}/{y}/{x}.png",
+    //["wind"]: "https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/Aquarius_Wind_Speed_L3_7Day_Snapshot/default/2014-04-09/GoogleMapsCompatible_Level6/{z}/{y}/{x}.png",
+    //
+    ["wind"]: "https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/CYGNSS_L3_Wind_Speed_Daily/default/default/GoogleMapsCompatible_Level6/{z}/{y}/{x}.png",
+    //["temp"]: "https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/AIRS_L3_Surface_Air_Temperature_Monthly_Night/default/2014-04-09/GoogleMapsCompatible_Level6/{z}/{y}/{x}.png",
+    ["temp"]: "https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/AIRS_L2_Surface_Skin_Temperature_Day/default/default/GoogleMapsCompatible_Level6/{z}/{y}/{x}.png",
 }
 
