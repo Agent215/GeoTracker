c13737585159e5a9430b6f7d6faa9f62864a7a17
roburdash <roburdash@gmail.com>
2020-11-21 12:43:07 -0500

    test and snapshot for weatheroverlay component

--- components/__tests__/WeatherOverlay-test.js
+++ components/__tests__/WeatherOverlay-test.js
@@ -12,15 +12,21 @@ enzyme.configure({ adapter: new Adapter() });
 const categories = ["clouds", "temp", "precipitation", "wind", "pressure"]
 
 describe('<WeatherOverlay />', () => {
-    it(`WeatherOverlay renders Correctly`, () => {
+
+    it(`WeatherOverlay renders Correctly on Load`, () => {
 
         const weatherOverlayComponent = renderer.create(<WeatherOverlay />).toJSON()    
         expect(weatherOverlayComponent).toMatchSnapshot();
         });
 
+    it('WeatherOverlay is null if no weather to be displayed', () => {
+
+        const weatherOverlayComponent = renderer.create(<WeatherOverlay category={"none"} gibsVisible={true} />).toJSON();
+    })
+
     it(`WeatherOverlay is not null if weather to be displayed`, () => {
 
-        const weatherOverlayComponent = renderer.create(<WeatherOverlay category={"clouds"} gibsVisible={false} />).toJSON()   
+        const weatherOverlayComponent = renderer.create(<WeatherOverlay category={ categories[Math.floor(Math.random() * categories.length)] } gibsVisible={false} />).toJSON()   
         expect(weatherOverlayComponent).not.toBeNull();
         });
 
