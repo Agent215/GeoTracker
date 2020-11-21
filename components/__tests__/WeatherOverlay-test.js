f1de4e659420605e6d439a8ede080984a7d7af31
roburdash <roburdash@gmail.com>
2020-11-21 12:28:08 -0500

    further tests for weatherOverlay

--- components/__tests__/WeatherOverlay-test.js
+++ components/__tests__/WeatherOverlay-test.js
@@ -1,44 +1,32 @@
 import * as React from 'react';
 import renderer from 'react-test-renderer';
-import CustomModal from '../../components/CustomModal';
+import WeatherOverlay from '../../components/WeatherOverlay';
 import * as enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { Provider } from 'react-redux'
 import configureStore from 'redux-mock-store'
 
-
 enzyme.configure({ adapter: new Adapter() });
 
-// mock of a prop to pass WeatherOverlay on Load
-const mockPropLoad = {
-    category: '',
-    gibsVisible: false,
-};
+//array of categories to test randomly
+const categories = ["clouds", "temp", "precipitation", "wind", "pressure"]
 
-//array to test category
-const categoryToggle = []
+describe('<WeatherOverlay />', () => {
+    it(`WeatherOverlay renders Correctly`, () => {
 
-//array to test gibsVisible
-const gibsVisibleToggle = [true, false];
+        const weatherOverlayComponent = renderer.create(<WeatherOverlay />).toJSON()    
+        expect(weatherOverlayComponent).toMatchSnapshot();
+        });
 
+    it(`WeatherOverlay is not null if weather to be displayed`, () => {
 
+        const weatherOverlayComponent = renderer.create(<WeatherOverlay category={"clouds"} gibsVisible={false} />).toJSON()   
+        expect(weatherOverlayComponent).not.toBeNull();
+        });
 
+    it('WeatherOVerlay is null if gibs is visible', () => {
 
-describe('Testing WeatherOverlay Component', () => {
-    const initialState = { output: "null" }
-    const mockStore = configureStore()
-    let store
-
-    //check that WeatherOverlay renders null on load
-    it(`WeatherOverlay renders correctly on load`, () => {
-        store = mockStore(initialState)
-        const comp = <Provider store={store}><CustomModal category={mockPropLoad.category} gibsVisible={mockPropLoad.gibsVisible} /></Provider>
-        const tree = renderer.create().toJSON(comp);
-
-        expect(tree).toBeNull();
-    });
-
-    //check that WeatherOverlay changes
-
-    //test random toggle of gibsVisible
+        const weatherOverlayComponent = renderer.create(<WeatherOverlay category={"none"} />).toJSON()
+        expect(weatherOverlayComponent).toBeNull();
+    })
 })
